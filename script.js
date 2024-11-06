472f44f64ed1204c9f0ad53ae44314ce97ce5a50
Patrycja Kalita <pati.kalita@gmail.com>
2024-11-06 16:51:53 +0100

    Services

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function responsible for animating the button when clicked
 var animateButton = function(e) {
 
     e.preventDefault;
@@ -14,3 +15,23 @@ var animateButton = function(e) {
   for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
   }
+
+// Handle tab switching in the 'services' section
+    const tabButtons = document.getElementsByClassName('service-tab');
+    const contents = document.getElementsByClassName('service-container');
+  
+    function switchTab(event) {
+          for (let i = 0; i < tabButtons.length; i++) {
+              tabButtons[i].classList.remove('active');
+              contents[i].classList.remove('active-content');
+          }
+
+          event.target.classList.add('active');
+  
+          const tabId = event.target.getAttribute('data-tab');
+          document.getElementById(`content-${tabId}`).classList.add('active-content');
+      }
+
+      for (let i = 0; i < tabButtons.length; i++) {
+          tabButtons[i].addEventListener('click', switchTab);
+      }
