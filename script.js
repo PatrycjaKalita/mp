2e43ae8ebfa1206787f9f3c8c5102bd29818060c
Patrycja Kalita <pati.kalita@gmail.com>
2024-11-07 12:16:32 +0100

    Services update

--- script.js
+++ script.js
@@ -8,30 +8,30 @@ var animateButton = function(e) {
     setTimeout(function(){
       e.target.classList.remove('animate');
     },700);
-  };
+};
   
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
   
-  for (var i = 0; i < bubblyButtons.length; i++) {
+for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
+}
 
 // Handle tab switching in the 'services' section
-    const tabButtons = document.getElementsByClassName('service-tab');
-    const contents = document.getElementsByClassName('service-container');
+const tabButtons = document.getElementsByClassName('service-tab');
+const contents = document.getElementsByClassName('service-container');
   
-    function switchTab(event) {
-          for (let i = 0; i < tabButtons.length; i++) {
-              tabButtons[i].classList.remove('active');
-              contents[i].classList.remove('active-content');
-          }
-
-          event.target.classList.add('active');
-  
-          const tabId = event.target.getAttribute('data-tab');
-          document.getElementById(`content-${tabId}`).classList.add('active-content');
-      }
-
+function switchTab(event) {
       for (let i = 0; i < tabButtons.length; i++) {
-          tabButtons[i].addEventListener('click', switchTab);
+          tabButtons[i].classList.remove('active');
+          contents[i].classList.remove('active-content');
       }
+
+      event.target.classList.add('active');
+  
+      const tabId = event.target.getAttribute('data-tab');
+      document.getElementById(`content-${tabId}`).classList.add('active-content');
+}
+
+for (let i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].addEventListener('click', switchTab);
+}
